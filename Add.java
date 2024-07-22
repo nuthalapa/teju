c25659986e2c342917ac341bb0b4f6ce7af92a9e
nuthalapa <167918765+nuthalapa@users.noreply.github.com>
2024-07-22 16:31:22 -0500

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Add {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        int c = a + b;
+        System.out.println("The addition of two numbers is : " + c);
+    }
+}
